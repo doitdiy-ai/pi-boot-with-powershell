6475b3f93a5f59e0e686b6c19039c83258cb37f5
doitdiy-ai <mello@doitdiy.ai>
2021-07-05 21:44:44 -0500

    Removed some unnecessary comments

--- boot-config.ps1
+++ boot-config.ps1
@@ -5,7 +5,7 @@ Add-Type -AssemblyName System.Drawing
 $script:SDCard = $true
 
 
-  $TZtoOlson = @"
+$TZtoOlson = @"
 W. Central Africa Standard Time = Africa/Bangui
 Egypt Standard Time = Africa/Cairo
 Morocco Standard Time = Africa/Casablanca
@@ -341,28 +341,12 @@ public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 function Show-Console
 {
     $consolePtr = [Console.Window]::GetConsoleWindow()
-
-    # Hide = 0,
-    # ShowNormal = 1,
-    # ShowMinimized = 2,
-    # ShowMaximized = 3,
-    # Maximize = 3,
-    # ShowNormalNoActivate = 4,
-    # Show = 5,
-    # Minimize = 6,
-    # ShowMinNoActivate = 7,
-    # ShowNoActivate = 8,
-    # Restore = 9,
-    # ShowDefault = 10,
-    # ForceMinimized = 11
-
     [Console.Window]::ShowWindow($consolePtr, 4)
 }
 
 function Hide-Console
 {
     $consolePtr = [Console.Window]::GetConsoleWindow()
-    #0 hide
     [Console.Window]::ShowWindow($consolePtr, 0)
 }
 
